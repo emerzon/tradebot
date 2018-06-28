4625074385128d76bae129808e8f2e6eb734876b
Emerson Gomes <emerson.gomes@gmail.com>
2018-06-28 21:03:18 +0200

    checkpoint 2

--- tb3.py
+++ tb3.py
@@ -13,6 +13,12 @@ def find_market_pairs():
         pairs.update({coin["Symbol"]: markets})
     return pairs
 
+def find_market_id(coin1, coin2):
+
+    for item in trade_pairs:
+        if item["Label"] == "%s/%s" %(coin1, coin2) or item["Label"] == "%s/%s" % (coin2, coin1):
+            return item["Id"]
+
 
 trade_pairs = requests.get("https://www.cryptopia.co.nz/api/GetTradePairs").json()['Data']
 
@@ -23,28 +29,29 @@ for coin, markets in coin_pairs.iteritems():
 
     for buying_market in markets:
         for selling_market in markets:
-            if buying_market != selling_market and coin != buying_market and coin != selling_market:
-                if "%s/%s" % (coin, buying_market) in trade_pairs:
-                    if "%s/%s" % (coin, selling_market) in trade_pairs:
-                        if "%s/%s" % (selling_market, buying_market) in trade_pairs:
-                            print " .. Simulating %s<%s, %s>%s, %s>%s" % (coin, buying_market, coin, selling_market, selling_market, buying_market)
+            if buying_market != selling_market and buying_market != coin and selling_market != coin:
+                print " .. Simulating %s>%s, %s>%s, %s>%s" % (buying_market, coin, coin, selling_market, selling_market, buying_market)
+                markets_to_query = "%s-%s-%s" % (find_market_id(coin, buying_market), find_market_id(coin, selling_market), find_market_id(selling_market, buying_market))
+                orders = requests.get("https://www.cryptopia.co.nz/api/GetMarketOrderGroups/%s/1" % markets_to_query).json()
 
-                            markets_to_query = "%s_%s-%s_%s-%s_%s" % (coin, buying_market, coin, selling_market, selling_market, buying_market)
+                for market in orders["Data"]:
+                    print market["Market"]
 
-                            orders = requests.get(
-                                "https://www.cryptopia.co.nz/api/GetMarketOrderGroups/%s/1" % markets_to_query).json()
+                    if market["Market"] == "%s_%s" % (buying_market, coin):
+                        step1_order_volume = market["Buy"][0]["Volume"]
+                    if market["Market"] == "%s_%s" % (coin, buying_market):
+                        step1_order_volume = market["Sell"][0]["Volume"]
 
-                            for market in orders["Data"]:
-                                print market["Market"]
-                            #if market["Market"] == "%s_%s" % (buying_market, coin):
-                            #print item["Data"]
-                            #if item["Data"][0]["Market"] ==
-                            #    step1_order_volume = item["Buy"][0]["Volume"]
-                            #elif item["Market"] == "%s_%s" % (coin, selling_market):
-                            #    step2_order_volume = item["Buy"][0]["Volume"]
-                            #else:
-                            #    step3_order_volume = item["Sell"][0]["Volume"]
+                    if market["Market"] == "%s_%s" % (coin, selling_market):
+                        step2_order_volume = market["Buy"][0]["Volume"]
+                    if market["Market"] == "%s_%s" % (selling_market, coin):
+                        step2_order_volume = market["Sell"][0]["Volume"]
 
-                            #order_size = min(step1_order_volume, step2_order_volume, step3_order_volume)
+                    if market["Market"] == "%s_%s" % (selling_market, buying_market):
+                        step3_order_volume = market["Sell"][0]["Volume"]
+                    if market["Market"] == "%s_%s" % (buying_market, selling_market):
+                        step3_order_volume = market["Buy"][0]["Volume"]
 
-                            #print "Order size: %0.8f (%0.8f, %0.8f, %0.8f)" % (order_size, step1_order_volume, step2_order_volume, step3_order_volume)
+                order_size = min(step1_order_volume, step2_order_volume, step3_order_volume)
+
+                print "Order size: %0.8f (%0.8f, %0.8f, %0.8f)" % (order_size, step1_order_volume, step2_order_volume, step3_order_volume)
