4845c65fd5c6bcf7ba595d577c7a283bfc0ee318
Emerson Gomes <emerson.gomes@gmail.com>
2018-07-21 16:45:23 +0200

    ta ficando grande

--- tb4.py
+++ tb4.py
@@ -1,5 +1,6 @@
 import requests
 import winsound
+import pprint
 
 import logging
 
@@ -13,6 +14,7 @@ logging.basicConfig(
     level=logging.DEBUG)
 
 logger = logging.getLogger(__name__)
+
 
 def find_market_pairs():
     if "trade_pairs" not in globals():
@@ -41,77 +43,163 @@ def find_market_id(coin1, coin2):
 
 
 def assemble_order_quotation(initial_quantity, *pairs):
-    quantity = 0
     market_ids = []
+    resulting_orders = []
+    quantity = initial_quantity
+
     for coin1, coin2 in pairs:
         market_ids.append(find_market_id(coin1, coin2)[0])
 
+    logger.debug("[..] Beggining order assembly...")
     logger.debug(
-        "Querying total of %s markets: (%s) [Max Orders: %s]" % (len(market_ids), "-".join(str(x) for x in market_ids), max_orders))
+        "[..] Querying total of %s markets: (%s) [Max Orders: %s]" % (
+            len(market_ids), ", ".join(str(x) for x in market_ids), max_orders))
     orders = s.get(
-        "https://www.cryptopia.co.nz/api/GetMarketOrderGroups/%s/%s" % ("-".join(str(x) for x in market_ids), max_orders)).json()
-
-    print orders
+        "https://www.cryptopia.co.nz/api/GetMarketOrderGroups/%s/%s" % (
+            "-".join(str(x) for x in market_ids), max_orders)).json()
 
     for coin1, coin2 in pairs:
-        if quantity == 0:
-            quantity = initial_quantity
-
-        logger.debug("------> Starting order assembly: %s %s -> %s " % (quantity, coin1, coin2))
+        logger.debug("[....] Starting suborder assembly: %s %s -> %s " % (quantity, coin1, coin2))
         market_id, reversed_market = find_market_id(coin1, coin2)
+        total_order_increase = float(0)
 
         for market in orders["Data"]:
             if market["TradePairId"] == market_id:
-                market_MinimumTrade = trade_pairs[market_id]["MinimumTrade"]
-                market_MinimumBaseTrade = trade_pairs[market_id]["MinimumBaseTrade"]
-                market_TradeFee = trade_pairs[market_id]["TradeFee"]
-                market_BaseSymbol = trade_pairs[market_id]["BaseSymbol"]
 
-                logger.debug("TradePairId is %s [MinimumTrade: %s - MiniminumBaseTrade %s" % (
-                market_id, market_MinimumTrade, market_MinimumBaseTrade))
+                resulting_suborders = []
+                suborder_price = float(0)
+                suborder_volume = float(0)
+
+                for item in trade_pairs:
+                    if market_id == item["Id"]:
+                        market_MinimumTrade = item["MinimumTrade"]
+                        market_MinimumBaseTrade = item["MinimumBaseTrade"]
+                        market_TradeFee = item["TradeFee"]
+                        market_BaseSymbol = item["BaseSymbol"]
+
+                logger.debug(
+                    "[....] TradePairId is {0} [MinimumTrade: {1:.20g} - MiniminumBaseTrade {2:.20g}]".format(market_id,
+                                                                                                      market_MinimumTrade,
+                                                                                                      market_MinimumBaseTrade))
+
+
+
 
                 if reversed_market:
                     direction = "Sell"
-                    logger.debug("Sell Order")
+                    logger.debug("[....] Sell Order")
+                    if quantity < market_MinimumBaseTrade:
+                        raise Exception('Order too low')
+
+                    volume_trade_fee = 1
+                    price_trade_fee = 1 + (market_TradeFee / 100)
                 else:
                     direction = "Buy"
-                    logger.debug("Sell Order")
+                    logger.debug("[....] Buy Order")
+                    if quantity < market_MinimumTrade:
+                        raise Exception('Order too low')
+                    price_trade_fee = 1
+                    volume_trade_fee = 1 - (market_TradeFee / 100)
 
-                qtd_orders = 0
-                order_grand_volume = 0
-                order_grand_price = 0
+                while ((suborder_price < quantity and direction == "Sell") or
+                       (suborder_volume < quantity and direction == "Buy")) and \
+                        len(resulting_suborders) < max_orders and \
+                        len(resulting_suborders) < len(market[direction]):
 
-                while ((order_grand_price < quantity and direction == "Sell") or
-                       (order_grand_volume < quantity and direction == "Buy")) and \
-                        qtd_orders < max_orders and \
-                        qtd_orders < len(market[direction]):
+                    logger.debug(
+                        "[......] Current suborder [{0:.20g}]: Total Price: {1:.20g} -  Total Volume: {2:.20g} - Total Requested: {3:.20g} {4}".format(
+                            len(resulting_suborders) + 1,
+                            suborder_price,
+                            suborder_volume,
+                            quantity,
+                            coin1))
 
-                    qtd_orders += 1
+                    current_volume = float(market[direction][len(resulting_suborders)]["Volume"] * volume_trade_fee)
+                    current_price = float(market[direction][len(resulting_suborders)]["Total"] * price_trade_fee)
 
-                    if order_grand_volume + market[direction][qtd_orders]["Volume"] <= quantity:
-                        logger.debug("Adding WHOLE order: %s -> %s (Unit price: %s)" % (
-                        market[direction][qtd_orders]["Volume"], market[direction][qtd_orders]["Total"],
-                        market[direction][qtd_orders]["Price"]))
-                        order_grand_volume += market[direction][qtd_orders]["Volume"]
-                        order_grand_price += market[direction][qtd_orders]["Total"]
+                    if (suborder_volume + current_volume <= quantity and direction == "Buy") or \
+                            (suborder_price + current_price <= quantity and direction == "Sell"):
+                        logger.debug(
+                            "[........] Adding WHOLE order: {0:.20g} -> {1:.20g} (Unit price: {2:.20g})".format(current_price,
+                                                                                                         current_volume,
+                                                                                                         market[direction][
+                                                                                                                   len(
+                                                                                                                       resulting_suborders)][
+                                                                                                                   "Price"]))
+                        order_filling_ratio = 1
                     else:
-                        logger.debug("Adding PARTIAL order... #TODO")
-                        order_grand_volume += market[direction][qtd_orders]["Volume"]
-                        order_grand_price += market[direction][qtd_orders]["Total"]
+                        if direction == "Sell":
+                            missing_price = quantity - suborder_price
+                            if missing_price < market_MinimumBaseTrade:
+                               missing_price = market_MinimumBaseTrade
 
-                    if not reversed_market:
+                            order_filling_ratio = missing_price / current_price
+                        else:
+                            missing_volume = quantity - suborder_volume
+                            if missing_volume < market_MinimumTrade:
+                                missing_volume = market_MinimumTrade
 
-                        order_grand_price *= 1 + market_TradeFee
-                    else:
-                        logger.debug("Sell Order")
-                        order_grand_volume *= 1 - market_TradeFee
+                            order_filling_ratio = missing_volume / current_volume
 
-    return ([order_grand_volume, order_grand_price, qtd_orders])
+                        logger.debug(
+                            "[........] Adding PARTIAL ({0:.3}%) order: {1:.20g} -> {2:.20g} (Unit price: {3:.20g})".format(
+                                order_filling_ratio*100,
+                                current_price * order_filling_ratio,
+                                current_volume * order_filling_ratio,
+                                float(market[
+                                          direction][
+                                          len(
+                                              resulting_suborders)][
+                                          "Price"])))
+
+                    resulting_suborders.append([market_id, direction, current_price * order_filling_ratio, current_volume * order_filling_ratio])
+
+                    suborder_price += current_price * order_filling_ratio
+                    suborder_volume += current_volume * order_filling_ratio
+        resulting_orders += resulting_suborders
+
+        if direction == "Sell":
+            logger.debug("[....] FINAL SUBORDER is {0:.20g} {1} -> {2:.20g} {3}".format(suborder_price, coin1, suborder_volume, coin2))
+            quantity = suborder_volume
+        else:
+            logger.debug("[....] FINAL SUBORDER is {2:.20g} {1} -> {0:.20g} {3}".format(suborder_price, coin1, suborder_volume, coin2))
+            quantity = suborder_price
+
+    return ({"Profit": quantity - initial_quantity,
+             "Orders": resulting_orders})
+
+minimum_order = {"BTC": 0.005,
+                 "LTC": 0.01,
+                 "DOGE": 100,
+                 "USDT": 1,
+                 "NZDT": 1}
 
 
 global s
 s = requests.Session()
 coin_pairs = find_market_pairs()
-max_orders = 10
+max_orders = 50
 
-print assemble_order_quotation(1, ["BTC", "LTC"])
+
+while True:
+    for coin, markets in coin_pairs.iteritems():
+        logging.info("Coin %s has %s available markets: (%s)" % (coin, len(markets), ", ".join(markets)))
+
+        for initial_market in markets:
+            for intermediary_market in markets:
+                if initial_market != intermediary_market and \
+                        initial_market != coin and \
+                        intermediary_market != coin:
+                    logging.info(" .. Simulating %s>%s, %s>%s, %s>%s" % (
+                        initial_market, coin, coin, intermediary_market, intermediary_market, initial_market))
+
+                    trade = assemble_order_quotation(minimum_order[initial_market], [initial_market, coin], [coin, intermediary_market], [intermediary_market, initial_market])
+                    print trade["Profit"]
+
+
+
+
+
+
+
+
