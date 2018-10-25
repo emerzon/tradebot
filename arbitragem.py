c1e963ac62a6ba6f642475015a153e394aecbc0d
Emerson Gomes <emerson.gomes@gemalto.com>
2018-10-25 18:22:28 +0200

    better way to compare?

--- arbitragem.py
+++ arbitragem.py
@@ -2,6 +2,7 @@
 import requests
 import datetime
 import time
+import itertools
 from decimal import *
 
 # Opcoes
@@ -30,19 +31,13 @@ def fetch_fiat(coin):
 
 def init_markets():
     global exchanges
-    lista = []
+
 
     # TradeOgre
     TradeOgre_markets = dlc("https://tradeogre.com/api/v1/markets")
     Cryptopia_markets = dlc("https://www.cryptopia.co.nz/api/GetTradePairs")["Data"]
     Poloniex_markets = dlc("https://poloniex.com/public?command=returnTicker")
     LiquiIO_markets = dlc("https://api.liqui.io/api/3/info")
-
-
-    for market in TradeOgre_markets:
-        for k, v in market.iteritems():
-            lista.append(k)
-    exchanges["TradeOgre"] = lista
 
     lista = []
     for market in Cryptopia_markets:
@@ -51,14 +46,20 @@ def init_markets():
     exchanges["Cryptopia"] = lista
 
     lista = []
+    for k, v in LiquiIO_markets["pairs"].iteritems():
+        lista.append("%s-%s" % (k.split("_")[1].upper(), k.split("_")[0].upper()))
+    exchanges["LiquiIO"] = lista
+
+    lista = []
     for market in Poloniex_markets:
         lista.append("%s-%s" % (market.split("_")[0], market.split("_")[1]))
     exchanges["Poloniex"] = lista
 
     lista = []
-    for k, v in LiquiIO_markets["pairs"].iteritems():
-        lista.append("%s-%s" % (k.split("_")[1].upper(), k.split("_")[0].upper()))
-    exchanges["LiquiIO"] = lista
+    for market in TradeOgre_markets:
+        for k, v in market.iteritems():
+            lista.append(k)
+    exchanges["TradeOgre"] = lista
 
 def get_values(exchange, market):
     if exchange == "TradeOgre":
@@ -103,41 +104,39 @@ init_markets()
 
 while True:
 
-    for xc1 in exchanges:
-        for xc2 in exchanges:
-            if xc1 != xc2 and xc1 < xc2:
-                print "Comparing %s with %s" % (xc1, xc2)
-                common_mkt = list(set(exchanges[xc1]).intersection(exchanges[xc2]))
-                print "...%s common markets" % len(common_mkt)
-                for mkt in common_mkt:
-                    #print "Probing %s" % mkt
-                    xc1_offer = get_values(xc1, mkt)
-                    xc1_buy = float(xc1_offer["Buy"])
-                    xc1_sell = float(xc1_offer["Sell"])
-                    xc2_offer = get_values(xc2, mkt)
-                    xc2_buy = float(xc2_offer["Buy"])
-                    xc2_sell = float(xc2_offer["Sell"])
+    for xc1, xc2 in itertools.combinations(exchanges, 2):
+        print "Comparing %s with %s" % (xc1, xc2)
+        common_mkt = list(set(exchanges[xc1]).intersection(exchanges[xc2]))
+        print "...%s common markets" % len(common_mkt)
+        for mkt in common_mkt:
+            #print "Probing %s" % mkt
+            xc1_offer = get_values(xc1, mkt)
+            xc1_buy = float(xc1_offer["Buy"])
+            xc1_sell = float(xc1_offer["Sell"])
+            xc2_offer = get_values(xc2, mkt)
+            xc2_buy = float(xc2_offer["Buy"])
+            xc2_sell = float(xc2_offer["Sell"])
 #                    print "[ %.2f%% ][ %.2f ] - [ %.2f ][ %.2f ]" % (xc1_buy, xc1_sell, xc2_buy, xc2_sell)
-                    if xc2_sell < xc1_buy and xc2_sell > 0 and xc1_buy >0:
-                        print "=== %s [ %s %.2f%% ] === Buy in %s for %.8f and sell in %s for %.8f!" % (
-                            time.strftime("%Y-%m-%d %H:%M"), mkt, (xc1_buy / xc2_sell * 100 - 100), xc2, xc2_sell,
-                            xc1, xc1_buy)
+            if xc2_sell < xc1_buy and xc2_sell > 0 and xc1_buy >0:
+                print "=== %s [ %s %.2f%% ] === Buy in %s for %.8f and sell in %s for %.8f!" % (
+                    time.strftime("%Y-%m-%d %H:%M"), mkt, (xc1_buy / xc2_sell * 100 - 100), xc2, xc2_sell,
+                    xc1, xc1_buy)
 
-                    if xc1_sell < xc2_buy and xc1_sell > 0 and xc2_buy >0:
-                        print "=== %s [ %s %.2f%% ] === Buy in %s for %.8f and sell in %s for %.8f!" % (
-                            time.strftime("%Y-%m-%d %H:%M"), mkt, (xc2_buy / xc1_sell * 100 - 100), xc1, xc1_sell,
-                            xc2, xc2_buy)
+            if xc1_sell < xc2_buy and xc1_sell > 0 and xc2_buy >0:
+                print "=== %s [ %s %.2f%% ] === Buy in %s for %.8f and sell in %s for %.8f!" % (
+                    time.strftime("%Y-%m-%d %H:%M"), mkt, (xc2_buy / xc1_sell * 100 - 100), xc1, xc1_sell,
+                    xc2, xc2_buy)
 
 #                    except:
 #                        pass
-                    # if xc2_sell <= xc1_buy or xc1_sell <= xc2_buy:
-                    #     print mkt
-                    #     print " %s: " % xc1
-                    #     print "  (-) : %.8f - US$ %.8f" % (xc1_buy, xc1_buy * fetch_fiat(mkt.split("-")[0]))
-                    #     print "  (+) : %.8f - US$ %.8f" % (xc1_sell, xc1_sell * fetch_fiat(mkt.split("-")[0]))
-                    #     print " %s: " % xc2
-                    #     print "  (-) : %.8f - US$ %.8f" % (xc2_buy, xc2_buy * fetch_fiat(mkt.split("-")[0]))
-                    #     print "  (+) : %.8f - US$ %.8f" % (xc2_sell, xc2_sell * fetch_fiat(mkt.split("-")[0]))
+            # if xc2_sell <= xc1_buy or xc1_sell <= xc2_buy:
+            #     print mkt
+            #     print " %s: " % xc1
+            #     print "  (-) : %.8f - US$ %.8f" % (xc1_buy, xc1_buy * fetch_fiat(mkt.split("-")[0]))
+            #     print "  (+) : %.8f - US$ %.8f" % (xc1_sell, xc1_sell * fetch_fiat(mkt.split("-")[0]))
+            #     print " %s: " % xc2
+            #     print "  (-) : %.8f - US$ %.8f" % (xc2_buy, xc2_buy * fetch_fiat(mkt.split("-")[0]))
+            #     print "  (+) : %.8f - US$ %.8f" % (xc2_sell, xc2_sell * fetch_fiat(mkt.split("-")[0]))
 
     print "------------------------------------------------------------------"
     time.sleep(30)
